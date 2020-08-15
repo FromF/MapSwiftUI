7dc4c38bce85e1692a25ed8a5d566d41f32b59be
Haruhito Fuji <haruhito.fuji@gmail.com>
2020-08-15 20:40:34 +0900

    SE-269対応

--- MapApp/MapApp/ContentView.swift
+++ MapApp/MapApp/ContentView.swift
@@ -34,30 +34,31 @@ struct ContentView: View {
                 let geocoder = CLGeocoder()
                 
                 // 入力された文字から位置情報を取得
-                geocoder.geocodeAddressString( self.keyword ) { (placemarks, error) in
+                geocoder.geocodeAddressString( keyword ) { (placemarks, error) in
                     // リクエストの結果が存在し、1件目の情報から位置情報を取り出す
                     if let placemarks = placemarks ,
                        let firstPlacemark = placemarks.first ,
                        let location = firstPlacemark.location {
                         
                         // 位置情報から緯度経度をtargetCoordinateに取り出す
-                        let coordinate = location.coordinate
+                        let pinCoordinate = location.coordinate
                         
                         // ピンの情報に置く場所の緯度経度を設定
-                        let annotationItem = AnnotationItemStruct(coordinate: coordinate)
+                        let annotationItem = AnnotationItemStruct(coordinate: pinCoordinate)
                         
                         // ピンを地図に置く
-                        self.annotationItems.append(annotationItem)
+                        annotationItems.append(annotationItem)
                         
                         // 緯度経度を中心にして半径500mの範囲を表示
-                        self.coordinate = MKCoordinateRegion(
-                            center: coordinate,
+                        coordinate = MKCoordinateRegion(
+                            center: pinCoordinate,
                             latitudinalMeters: 500.0,
                             longitudinalMeters: 500.0)
                     }
                 }
             }
             .textFieldStyle(RoundedBorderTextFieldStyle())
+            
             
             // 地図を表示
             Map(coordinateRegion: $coordinate, annotationItems: annotationItems) { annotationItem in
